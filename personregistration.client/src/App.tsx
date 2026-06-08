f801e359025006634c6a1b00e621c515841b760c
Mufado <gustavogiraobg@gmail.com>
2026-06-08 20:42:20 -0300

    Add project files.

---
+++ personregistration.client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+        </div>
+    );
+}
+
+export default App;
