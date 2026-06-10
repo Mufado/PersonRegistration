d8ce6b2f3d358e292b874a9cdc96002280287c45
Mufado <gustavogiraobg@gmail.com>
2026-06-09 23:19:08 -0300

    docs(backend): minor inline docs addition

--- PersonRegistration.Server/DTOs/V2/PersonRequest.cs
+++ PersonRegistration.Server/DTOs/V2/PersonRequest.cs
@@ -2,6 +2,13 @@
 
 public class PersonRequest
 {
+    // Required properties are not using the `required` modifier so the request
+    // can be model bound from JSON without the client having to specify all properties.
+    // The validation of required properties is handled separately using FluentValidation.
+    //
+    // BirthDate is nullable to allow clients to omit it from the request,
+    // but it is validated as required also using FluentValidation.
+
     public string Name { get; set; } = string.Empty;
     public string Cpf { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
